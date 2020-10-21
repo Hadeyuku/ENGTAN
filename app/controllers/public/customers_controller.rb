ef541ea7857b2adff69a26625d8d4771bd3a9e13
vagrant <nakagawahide35@gmail.com>
2020-10-21 05:31:46 +0000

    add mypage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,8 @@
 class Public::CustomersController < ApplicationController
     def show
-        
+        @engtan_words = Word.where(genre: 'ENGTAN')
+        @selftan_words = current_customer.words.where(genre: 'SELFTAN')
+        @total_words = current_customer.words
+        @words = current_customer.words.order("RANDOM()").limit(4)
     end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,11 +2,23 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
     has_many :test_words
     has_many :words
     has_many :tests
     has_many :favorites, dependent: :destroy
 
     enum status: { novice: 0, intermidiate: 1, advanced: 2}
+
+    def registered
+      where(status: 'registered')
+    end
+
+    def ongoing
+      where(status: 'ongoing')
+    end
+
+    def complete
+      where(status: 'complete')
+    end
 end
